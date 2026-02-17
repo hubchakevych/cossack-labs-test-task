907ceabb61d3e3bf37f2ceb12fccd5f87d5de293
serhii.hubchakevych <serhii.hubchakevych@softconstruct.com>
2026-02-17 02:18:40 +0200

    Update default values preparation in EditProductForm to handle undefined properties with fallback values.

--- src/features/edit-product/model/defaultValues.ts
+++ src/features/edit-product/model/defaultValues.ts
@@ -1,17 +1,10 @@
 import { Product } from '@/entities/product'
 
-export const defaultValues = {
-  title: '',
-  category: '',
-  price: 0,
-  stock: 0,
-}
-
 export const prepareDefaultValues = (product: Product) => {
   return {
-    title: product.title,
-    category: product.category,
-    price: product.price,
-    stock: product.stock,
+    title: product.title ?? '',
+    category: product.category ?? [],
+    price: product.price ?? 0,
+    stock: product.stock ?? 0,
   }
 }
